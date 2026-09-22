a3cbcba75739584482f56cc68fc08530b6f08a32
ihateito <art1998a@gmail.com>
2026-09-23 03:52:12 +0400

    Refactor Cell class to cell function

--- js/cell.js
+++ js/cell.js
@@ -1,10 +1,8 @@
 "use strict";
 
-class Cell {
-  constructor(i, j, value) {
-    this.i = i;
-    this.j = j;
-    this.value = value;
-    this.div = null;
-  }
+function cell(i, j, value) {
+  this.i = i;
+  this.j = j;
+  this.value = value;
+  this.div = null;
 }
