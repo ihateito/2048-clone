e8cee805e41553e8595499fd7c9c5217f82a2b55
ihateito <art1998a@gmail.com>
2026-09-23 03:28:01 +0400

    Refactor supportFunctions.js to use constants and arrow functions

--- js/supportFunctions.js
+++ js/supportFunctions.js
@@ -1,90 +1,65 @@
-"use strict"
+"use strict";
 
-var documentWidth = window.screen.width;
-var documentHeight = window.screen.height;
-var gridContainerWidth = 500;
-var cellSpace = 20;
-var cellSideLength = 100;
+const CELL_SPACE = 20;
+const CELL_SIDE_LENGTH = 100;
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+const COLOR_MAP = {
+  2: "#eee4da",
+  4: "#ede0c8",
+  8: "#f2b179",
+  16: "#f59563",
+  32: "#f67c5f",
+  64: "#ec6544",
+  128: "#e44d29",
+  256: "#edcf72",
+  512: "#c8a145",
+  1024: "#a8832b",
+  2048: "#86aa9c",
+  4096: "#a6c",
+  8192: "#791e6f"
+};
+
+const getRandomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+const generateValue = () => (Math.random() < 0.9 ? 2 : 4);
+
+const getPosTop = (i, j) => CELL_SPACE + i * (CELL_SPACE + CELL_SIDE_LENGTH);
+const getPosLeft = (i, j) => CELL_SPACE + j * (CELL_SPACE + CELL_SIDE_LENGTH);
+
+function getNumberCellFontSize(number) {
+  if (number <= 64) return `${0.6 * CELL_SIDE_LENGTH}px`;
+  if (number <= 512) return `${0.5 * CELL_SIDE_LENGTH}px`;
+  if (number <= 8192) return `${0.4 * CELL_SIDE_LENGTH}px`;
+  return `${0.3 * CELL_SIDE_LENGTH}px`;
 }
 
+const getNumberCellBgColor = (number) => COLOR_MAP[number] || "black";
 
-function generateValue() {
-  var percent = getRandomInt(0, 100);
-  if (percent >= 0 && percent <= 91)
-    return 2;
-  else
-    if (percent > 91 && percent <= 100)
-      return 4;
-}
+const getNumberCellFontColor = (number) => (number <= 4 ? "#776e65" : "white");
 
-function getPosTop(i, j){
-	return cellSpace + i * (cellSpace + cellSideLength);
-}
-
-function getPosLeft(i, j){
-	return cellSpace + j * (cellSpace + cellSideLength);
-}
-
-function getNumberCellFontSize(number){
-	if(number <= 64)
-		return 0.6 * cellSideLength + "px";
-	else if(number <= 512)
-		return 0.5 * cellSideLength + "px";
-	else if(number <=8192)
-		return 0.4 * cellSideLength + "px";
-	else
-		return 0.3 *cellSideLength + "px";
-	return "white";
-}
-
-function getNumberCellBgColor(number){
-	switch(number){
-		case 2:return "#eee4da"; break;
-		case 4:return "#ede0c8"; break;
-		case 8:return "#f2b179"; break;
-		case 16:return "#f59563"; break;
-		case 32:return "#f67c5f"; break;
-		case 64:return "#ec6544"; break;
-		case 128:return "#e44d29"; break;
-		case 256:return "#edcf72"; break;
-		case 512:return "#c8a145"; break;
-		case 1024:return "#a8832b"; break;
-		case 2048:return "#86aa9c"; break;
-		case 4096:return "#a6c"; break;
-		case 8192:return "#791e6f"; break;
-	}
-	return "black";
-}
-
-function getNumberCellFontColor(number){
-	if(number <= 4){
-		return "#776e65";
-	}
-	return "white";
-}
-
-
-function canMoveX(field) {
-  for (var i = 1; i <= size; i++)
-    for (var j = 1; j < size; j++) {
-      var cell = "" + i + j;
-      var nextCell = "" + i + (j + 1);
-      if (field[cell].value == field[nextCell].value)
+function canMoveX(fieldObj) {
+  for (let i = 1; i <= size; i++) {
+    for (let j = 1; j < size; j++) {
+      const current = fieldObj[`${i}${j}`];
+      const next = fieldObj[`${i}${j + 1}`];
+      if (current && next && current.value === next.value) {
         return true;
+      }
     }
+  }
   return false;
 }
 
-function canMoveY(field) {
-  for (var j = 1; j <= size; j++) 
-    for (var i = 1; i < size; i++) {
-      var cell = "" + i + j;
-      var nextCell = "" + (i + 1) + j;
-      if (field[cell].value == field[nextCell].value)
+function canMoveY(fieldObj) {
+  for (let j = 1; j <= size; j++) {
+    for (let i = 1; i < size; i++) {
+      const current = fieldObj[`${i}${j}`];
+      const next = fieldObj[`${i + 1}${j}`];
+      if (current && next && current.value === next.value) {
         return true;
+      }
     }
+  }
   return false;
 }
